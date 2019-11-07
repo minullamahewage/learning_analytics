3cabf385e48858284faf125c7fb69f77184c5198
hash <kumarasinghe.k.a.s.h@gmail.com>
2019-11-07 07:03:32 +0530

    teacher and student home pages UI

---
+++ Interface/studentProfile.php
@@ -0,0 +1,143 @@
+<!DOCTYPE HTML>
+<!--
+	Spectral by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Learning Analytics System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta http-equiv="Content-Language" content="en">
+		<link rel="stylesheet" href="../assets/css/main.css" />
+        <link rel="stylesheet" href="../assets/css/noscript.css" />
+        <link rel="stylesheet" href="../assets/css/card.css" />
+		<link rel="icon" href="../assets/graphics/app-icon-transparent.png">
+
+        <script>
+  window.console = window.console || function(t) {};
+</script>
+<script>
+  if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+</script>
+<script src="chrome-extension://mooikfkahbdckldjjndioackbalphokd/assets/prompt.js"></script>
+
+
+	</head>
+	<body class="landing is-preload">
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+				
+<div id="main-body">
+
+</div>
+					<header id="header" class="alt">
+						<h1><a href="index.html">LearningAnalytics</a></h1>
+						<nav id="nav">
+							<ul>
+								<li class="special">
+									<a href="#menu" class="menuToggle"><span>Menu</span></a>
+									<div id="menu">
+										<ul>
+											<li><a href="../index.html">Home</a></li>
+											<li><a href="../User/registration.php">Sign Up</a></li>
+											<li><a href="../User/login.php">Login</a></li>
+											<!--li><a href="reviews.php">Reviews</a></li-->
+											<li><a href="../Interface/about.html">About us</a></li>
+
+										</ul>
+									</div>
+								</li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Banner -->
+					
+                    <section >
+
+                    
+                        <center>
+                        <div class="inner">
+						    <br>
+						  <h2>School Management System</h2>
+							<p>
+                          <!-- <strong>Learning Analytics</strong> -->
+                        </p>
+                        <strong>Learning Analytics</strong> 
+                        </center>
+                    <div class="content">
+
+                        <a class="card" href="#">
+                        <div class="front" style="background-image: url(../assets/graphics/records.jpg);">
+                        <p>My records</p>
+                        </div>
+                        <div class="back">
+                        <div>
+                        <!-- <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+                        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p> -->
+                        <button class="button">View records</button>
+                        </div>
+                        </div></a><a class="card" href="#">
+                        <div class="front" style="background-image: url(../assets/graphics/attendance.jpg);">
+                        <p>My Attendance</p>
+                        </div>
+                        <div class="back">
+                        <div>
+                        <!-- <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+                        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p> -->
+                        <button class="button">View Attendance</button>
+                        </div>
+                        </div></a><a class="card" href="#">
+                        <div class="front" style="background-image: url(../assets/graphics/teachingmat.jpg);">
+                        <p>Student Materials</p>
+                        </div>
+                        <div class="back">
+                        <div>
+                        
+                        <button class="button">View Attendance</button>
+                        </div>
+                        </div></a><a class="card" href="#">
+                        <div class="front" style="background-image: url(../assets/graphics/results.jpg);">
+                        <p>My results</p>
+                        </div>
+                        <div class="back">
+                        <div>
+                        <button class="button">View my results</button>
+                        </div>
+                       
+                        </div></a>
+                        <a class="card" href="#">
+                        <div class="front" style="background-image: url(../assets/graphics/device.png);">
+                        <p>Device details</p>
+                        </div>
+                        <div class="back">
+                        <div>
+                        <!-- <p>Consectetur adipisicing elit. Possimus, praesentium?</p>
+                        <p>Provident consectetur natus voluptatem quis tenetur sed beatae eius sint.</p> -->
+                        <button class="button">Update device details</button>
+                        </div>
+                        </div></a>
+                        </div>
+                    </section>
+	
+                    
+  
+		<!-- Scripts -->
+			<script src="../assets/js/jquery.min.js"></script>
+			<script src="../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../assets/js/browser.min.js"></script>
+			<script src="../assets/js/breakpoints.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<script src="../assets/js/main.js"></script>
+
+
+	</body>
+</html>
