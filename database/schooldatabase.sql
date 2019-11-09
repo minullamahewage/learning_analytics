67999c516bcadf44c928ee0e82de4447fe57f2cf
Minul Lamahewage <minullamahewage@gmail.com>
2019-11-09 22:39:43 +0530

    fixed error

--- database/schooldatabase.sql
+++ database/schooldatabase.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 08, 2019 at 11:01 AM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Host: localhost
+-- Generation Time: Nov 09, 2019 at 06:09 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,52 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `schooldatabase`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courselist`
---
-
-CREATE TABLE `courselist` (
-  `courseid` varchar(10) NOT NULL,
-  `sid` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `courselist`
---
-
-INSERT INTO `courselist` (`courseid`, `sid`) VALUES
-('001', '1234S'),
-('001', '2345S'),
-('002', '2345S'),
-('005', '1234S');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `courses`
---
-
-CREATE TABLE `courses` (
-  `courseid` varchar(10) NOT NULL,
-  `coursename` varchar(50) NOT NULL,
-  `teacherid` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `courses`
---
-
-INSERT INTO `courses` (`courseid`, `coursename`, `teacherid`) VALUES
-('001', 'Computer Science', '1234T'),
-('002', 'Biology', '2345T'),
-('003', 'Mathematics', '3456T'),
-('004', 'Electrical En.', '4567T'),
-('005', 'Science', '1234T'),
-('006', 'English', '2345T'),
-('007', 'Archi', '2345T');
 
 -- --------------------------------------------------------
 
@@ -134,9 +88,20 @@ INSERT INTO `students` (`sid`, `sname`, `intavail`) VALUES
 --
 
 CREATE TABLE `student_attendance` (
+  `id` int(10) NOT NULL,
   `sid` varchar(10) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student_attendance`
+--
+
+INSERT INTO `student_attendance` (`id`, `sid`, `date`, `status`) VALUES
+(1, '170000A', '2019-11-09', 'absent'),
+(2, '170000A', '2019-11-10', 'present'),
+(3, '1234S', '2019-11-09', 'present');
 
 -- --------------------------------------------------------
 
@@ -167,58 +132,15 @@ INSERT INTO `teachers` (`tid`, `tname`, `intavail`) VALUES
 --
 
 CREATE TABLE `teacher_attendance` (
+  `id` int(10) NOT NULL,
   `tid` varchar(10) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `status` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `teacher_attendance`
---
-
-INSERT INTO `teacher_attendance` (`tid`, `date`) VALUES
-('1234T', '2019-07-21'),
-('2345T', '2019-07-04'),
-('1234T', '2019-07-23'),
-('1234T', '2019-07-29');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `userid` varchar(10) NOT NULL,
-  `uname` varchar(50) NOT NULL,
-  `upass` varchar(50) DEFAULT NULL,
-  `utype` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userid`, `uname`, `upass`, `utype`) VALUES
-('1234S', 'Test1', '5a105e8b9d40e1329780d62ea2265d8a', 'Student'),
-('1234T', 'Test2', 'ad0234829205b9033196ba818f7a872b', 'Teacher'),
-('2345T', 'Test4', '86985e105f79b95d6bc918fb45ec7727', 'Teacher'),
-('admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', 'Admin');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `courselist`
---
-ALTER TABLE `courselist`
-  ADD PRIMARY KEY (`courseid`,`sid`);
-
---
--- Indexes for table `courses`
---
-ALTER TABLE `courses`
-  ADD PRIMARY KEY (`courseid`);
 
 --
 -- Indexes for table `devices`
@@ -233,10 +155,26 @@ ALTER TABLE `results`
   ADD PRIMARY KEY (`sid`,`courseid`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `student_attendance`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userid`);
+ALTER TABLE `student_attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `teacher_attendance`
+--
+ALTER TABLE `teacher_attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student_attendance`
+--
+ALTER TABLE `student_attendance`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
